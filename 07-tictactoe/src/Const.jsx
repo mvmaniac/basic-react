ed01eb6d0d3f7a032871ccadf234278c95eb4a6c
yhs <mvmaniaz@gmail.com>
2024-07-05 00:17:04 +0900

    Delete old source

--- 07-tictactoe/src/Const.jsx
+++
@@ -1,4 +0,0 @@
-export const SET_WINNER = 'SET_WINNER';
-export const CLICK_CELL = 'CLICK_CELL';
-export const CHANGE_TURN = 'CHANGE_TURN';
-export const RESET_GAME = 'RESET_GAME';
--- 08-minesearch/src/MineSearchContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const MineSearchContext = createContext({
-  tableData: [],
-  halted: true,
-  dispatch: () => {},
-});
-
-export default MineSearchContext;
